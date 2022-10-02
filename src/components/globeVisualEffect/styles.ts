0290aaa5ad8b8143cba681391c6f77a825c4ce18
luiz toquetto <luiz.toquetto@facens.br>
2022-10-02 16:48:35 -0300

    feat(components): creating visual effect component

---
+++ src/components/globeVisualEffect/styles.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    
+    z-index: -1;
+    
+    position: absolute;
+  
+    bottom: 0;
+    right: 0;
+    left: 0;
+    top: 0;
+`;
+
+export { Container };
