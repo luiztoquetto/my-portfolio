a665743ed30fd92edd2c6322b971ea08e89bf4f3
luiz toquetto <luiz.toquetto@facens.br>
2022-10-02 16:13:53 -0300

    feat(routing): added routes

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+import * as S from './styles';
+
+export function Home(): ReactElement {
+    return (
+        <S.Container>olá mundo!</S.Container>
+    )
+}
